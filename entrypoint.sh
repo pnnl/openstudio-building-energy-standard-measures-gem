b9cd8bbd82817fdd79bedea20e9bcabb6f37f624
Julian Slane <julian.slane-holloway@pnnl.gov>
2024-12-04 12:18:01 -0800

    Output to test_results folder (it is finally working)

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,10 @@
 
 echo $GITHUB_WORKSPACE
 
-openstudio measure -t ./lib/measures >> $GITHUB_WORKSPACE/measure_check_output.txt
+mkdir $GITHUB_WORKSPACE/test_results
+
+openstudio measure -t ./lib/measures >> $GITHUB_WORKSPACE/test_results/measure_check_output.txt
 
 for dir in $(find . -type f -name 'measure.py' -exec dirname {} \; | sort -u); do
-    pytest "$dir" >> "$GITHUB_WORKSPACE/pytest_output.txt"
+    pytest "$dir" >> "$GITHUB_WORKSPACE/test_results/pytest_output.txt"
 done
