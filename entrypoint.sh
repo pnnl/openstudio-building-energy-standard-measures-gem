ab27c8ad60f949e0175017feada7b13abeed63d4
Julian Slane <julian.slane-holloway@pnnl.gov>
2024-12-04 12:13:41 -0800

    Fix entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ echo $GITHUB_WORKSPACE
 
 openstudio measure -t ./lib/measures >> $GITHUB_WORKSPACE/measure_check_output.txt
 
-for dir in \$(find . -type f -name 'measure.py' -exec dirname {} \; | sort -u); do \
-    pytest \$dir >> $GITHUB_WORKSPACE/pytest_output.txt; \
+for dir in $(find . -type f -name 'measure.py' -exec dirname {} \; | sort -u); do
+    pytest "$dir" >> "$GITHUB_WORKSPACE/pytest_output.txt"
 done
